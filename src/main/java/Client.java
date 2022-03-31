afa6e1e0628be812c53e836a0c7652ed3b1c98ff
mahdikarimi422@gmail.com <09388990352@>
2022-03-31 10:57:42 +0430

    client server

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,13 +4,15 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
 
-public class Server {
+public class Client {
+
     public static final int SERVER_PORT =998;
     public static final int CLIENT_PORT =999;
     public static final int BUFFER_SIZE =1024;
     public static DatagramSocket DS;
     public static final byte BUFFER[] =new byte[BUFFER_SIZE];
     public static final Scanner scanner=new Scanner(System.in);
+
 
     public static void receive() throws IOException {
         DatagramPacket p=new DatagramPacket(BUFFER, BUFFER.length);
@@ -25,17 +27,15 @@ public class Server {
         DS.send(new DatagramPacket(text.getBytes(),text.length(), InetAddress.getLoopbackAddress(),port));
     }
 
-    public static void theServer() throws IOException {
+    public static void TheClient() throws IOException {
         while (true){
-            send(CLIENT_PORT);
             receive();
+            send(SERVER_PORT);
         }
     }
 
-
     public static void main(String[] args) throws IOException {
-            DS = new DatagramSocket(SERVER_PORT);
-            theServer();
-
+        DS = new DatagramSocket(CLIENT_PORT);
+        TheClient();
     }
 }
